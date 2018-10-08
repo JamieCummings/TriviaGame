d2050fb52d9c6afb382f14447f9feda80bac9997
Jamie Cummings <jamiecummings@MacBook-Pro-2.local>
2018-10-08 12:34:23 -0500

    Adding initial project files

---
+++ Trivia Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trivia Game
+//
+//  Created by Jamie Cummings on 10/8/18.
+//  Copyright © 2018 Jamie Cummings. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
